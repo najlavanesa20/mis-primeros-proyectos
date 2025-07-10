a6c26ec12cb76fce87a6c4e4a368d5dbb7371c7c
Najla Vanesa <najlavanesa@gmail.com>
2025-07-09 22:19:00 -0600

    Primer commit: Agregué dia1.py y dia2.py

---
+++ dia1.py
@@ -0,0 +1,7 @@
+print("Hola, Najla")
+nombre=input("¿Cómo te llamas?")
+print("Hola, "+nombre)
+
+edad=int(input("¿Cuántos años tienes?"))
+año_100=2025-edad+100
+print("Cumplirás 100 años en el año", año_100)
---
+++ dia2.py
@@ -0,0 +1,6 @@
+#Programa que suma dos números ingresados por el usuario
+print("Suma de dos numeros")
+a=float(input("Dame el primer número: "))
+b=float(input("Dame el segundo número: "))
+suma=a+b
+print("La suma es: ", suma)
